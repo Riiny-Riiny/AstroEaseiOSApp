aad30cbbcff197ff90bbc17a17d305151d886280
Riiny Giir <rgiir2020@riordanhs.org>
2023-04-19 16:36:26 -0400

    First commit.

--- AstroEase/ViewController.swift
+++ AstroEase/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        
+        imageView.image = UIImage(named: "logo")
+        return imageView
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(imageView)
+        view.backgroundColor = .systemPink
+        imageView.center = view.center
+        
     }
+    
 
 
 }
